ed4ff7c94b568c1702bc1484fe398723745cebb2
Amaro Osses <killsxe123@gmail.com>
2025-08-29 13:11:47 -0400

    creacion tablas en models

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categoria, Zona, Dispositivo
+
+admin.site.register([Categoria,Zona])
+
+admin.site.register([Dispositivo])
+
